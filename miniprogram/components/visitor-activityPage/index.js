53e7bd415633b4dde114864ca3fd70bf2fc27710
乱了的和弦 <2324262941@qq.com>
2020-08-30 18:38:09 +0800

    游客活动页

--- miniprogram/components/visitor-activityPage/index.js
+++ miniprogram/components/visitor-activityPage/index.js
@@ -4,14 +4,25 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    option1: [
+      { text: '当前活动', value: 0 },
+      { text: '未开始活动', value: 1 },
+      { text: '已结束活动', value: 2 },
+    ],
+    option2: [
+      { text: '默认排序', value: 'a' },
+      { text: '热门排序', value: 'b' },
+      { text: '距离排序', value: 'c' },
+    ],
+    value1: 0,
+    value2: 'a',
   },
 
   /**
