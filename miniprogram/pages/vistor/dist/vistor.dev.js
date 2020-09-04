918ac6f6669574d3624a6296f3505dba5df9a546
hqy <hqy1012927920@163.com>
2020-09-04 17:05:21 +0800

    x

--- miniprogram/pages/vistor/dist/vistor.dev.js
+++ miniprogram/pages/vistor/dist/vistor.dev.js
@@ -5,27 +5,17 @@ Page({
   data: {
     active: 0,
     pageList: [{
-      text: '首页',
-      url: '../visitor-index/visitor-index'
+      text: '首页'
     }, {
-      text: '活动',
-      url: '../visitor-active/visitor-active'
+      text: '活动'
     }, {
-      text: '我的',
-      url: '../visitor-user/visitor-user'
+      text: '我的'
     }]
   },
   onChange: function onChange(event) {
     // event.detail 的值为当前选中项的索引
     this.setData({
       active: event.detail
-    });
-    wx.showToast({
-      title: "\u70B9\u51FB\u6807\u7B7E ".concat(event.detail + 1),
-      icon: 'none'
-    });
-    wx.navigateTo({
-      url: this.data.pageList[event.detail].url
     });
   }
 });
--- miniprogram/pages/vistor/vistor.js
+++ miniprogram/pages/vistor/vistor.js
@@ -4,28 +4,18 @@ Page({
     active: 0,
     pageList: [
       {
-        text: '首页',
-        url: '../visitor-index/visitor-index'
+        text: '首页'
       },
       {
-        text: '活动',
-        url: '../visitor-active/visitor-active'
+        text: '活动'
       },
       {
-        text: '我的',
-        url: '../visitor-user/visitor-user'
+        text: '我的'
       }
     ]
   },
   onChange(event) {
     // event.detail 的值为当前选中项的索引
     this.setData({ active: event.detail });
-    wx.showToast({
-      title: `点击标签 ${event.detail + 1}`,
-      icon: 'none',
-    });
-    wx.navigateTo({
-      url: this.data.pageList[event.detail].url
-    });
   },
 });
